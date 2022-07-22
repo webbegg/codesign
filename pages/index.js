e0659c3dcc60d70ca67ec3da1b764c571ab8461f
Marcos Juan <webbegg@gmail.com>
2022-07-22 20:58:03 +0200

    add font-face

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ export default function Home() {
       </Head>
 
       <main className="h-screen flex justify-center items-center bg-gray-900 select-none">
-        <h1 className="cursor-default flex flex-col font-bold leading-none text-2xl text-gray-300 bg-gray-800 shadow-lg p-4 rounded-2xl">
+        <h1 className="font-title tracking-wide leading-none cursor-default flex flex-col text-4xl text-gray-300 bg-gray-800 shadow-lg p-4 rounded-2xl">
           <span>
-            CODE <span className="text-gray-500">&</span>
+            CODE <span className="text-gray-700">&</span>
           </span>
           <span>DESIGN</span>
         </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
+        title: ["'Cubano'"],
         body: ["'Inter'"],
       },
     },
